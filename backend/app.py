847406e889d3435238e1172f3ac164f9922988da
22f3001988 <22f3001988@ds.study.iitm.ac.in>
2025-10-31 15:04:06 +0530

    Updated backend

--- backend/app.py
+++ backend/app.py
@@ -1,23 +1,31 @@
 from flask import Flask
-from flask_restful import Api, Resource
 from flask_cors import CORS
 
 app = Flask(__name__)
-api = Api(app)
 CORS(app)
 
+# connecting to database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-from models import db
+from models import db, User
 db.init_app(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {"message": "Hello world!"}
-    
-api.add_resource(HelloWorld, '/message')
+# connecting to routes
+from routes import api
+api.init_app(app)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    
     with app.app_context():
-        db.create_all()
+
+        db.create_all()  # Create database tables for our data models
+
+        admin = User.query.filter_by(email='admin@gmail.com').first()
+
+        if not admin:
+            admin = User(email='admin@gmail.com', password='admin', role='admin')
+            db.session.add(admin); db.session.commit()
+            print("Admin user created with email: admin@gmail.com and password: admin")
+        else:
+            print("Admin user already exists with email: admin@gmail.com and password: admin")
         
     app.run(debug=True)
